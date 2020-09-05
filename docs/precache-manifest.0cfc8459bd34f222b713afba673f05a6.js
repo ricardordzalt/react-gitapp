a790c5c0fe128a049d78093ca15b8a1fb3d36d6d
ricardordzalt <ricardo.rdzalt@gmail.com>
2020-09-04 21:51:01 -0500

    faltó 1 chunk.js

---
+++ docs/precache-manifest.0cfc8459bd34f222b713afba673f05a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4c8e820cc9a8d663f9a41fdc3d11ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "870f2aef481f785c883e",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "b9b37e3fbf9fa3ca719f",
+    "url": "/static/css/main.ce2c065b.chunk.css"
+  },
+  {
+    "revision": "870f2aef481f785c883e",
+    "url": "/static/js/2.30c3c308.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.30c3c308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b37e3fbf9fa3ca719f",
+    "url": "/static/js/main.d7508c0a.chunk.js"
+  },
+  {
+    "revision": "19fd0df589273590b438",
+    "url": "/static/js/runtime-main.fa546c3d.js"
+  }
+]);
